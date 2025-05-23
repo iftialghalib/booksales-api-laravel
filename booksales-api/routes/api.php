c6b183d5d3e3b94423ad23cd1c9ace44997397fb
iftialghalib <niceoneforyou11@gmail.com>
2025-05-24 00:02:40 +0700

    menambahakan update& delete api

--- booksales-api/routes/api.php
+++ booksales-api/routes/api.php
@@ -11,26 +11,30 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::get('/Author', [AuthorController::class, 'index']);
-Route::post('/Author', [AuthorController::class, 'store']);
-Route::get('/Author/{id}', [AuthorController::class, 'show']);
-Route::post('/Author/{id}', [AuthorController::class, 'update']);
-Route::delete('/Author/{id}', [AuthorController::class, 'destroy']);
+// Route::get('/Author', [AuthorController::class, 'index']);
+// Route::post('/Author', [AuthorController::class, 'store']);
+// Route::get('/Author/{id}', [AuthorController::class, 'show']);
+// Route::post('/Author/{id}', [AuthorController::class, 'update']);
+// Route::delete('/Author/{id}', [AuthorController::class, 'destroy']);
 
 
 
-Route::get('/Genre', [GenreController::class, 'index']);
-Route::post('/Genre', [GenreController::class, 'store']);
-Route::get('/Genre/{id}', [GenreController::class, 'show']);
-Route::post('/Genre/{id}', [GenreController::class, 'update']);
-Route::delete('/Genre/{id}', [GenreController::class, 'destroy']);
+// Route::get('/Genre', [GenreController::class, 'index']);
+// Route::post('/Genre', [GenreController::class, 'store']);
+// Route::get('/Genre/{id}', [GenreController::class, 'show']);
+// Route::post('/Genre/{id}', [GenreController::class, 'update']);
+// Route::delete('/Genre/{id}', [GenreController::class, 'destroy']);
 
 
 
-Route::get('/Book', [BookController::class, 'index']);
-Route::post('/Book', [BookController::class, 'store']);
-Route::get('/Book/{id}', [BookController::class, 'show']);
-Route::post('/Book/{id}', [BookController::class, 'update']);
-Route::delete('/Book/{id}', [BookController::class, 'destroy']);
+// Route::get('/Book', [BookController::class, 'index']);
+// Route::post('/Book', [BookController::class, 'store']);
+// Route::get('/Book/{id}', [BookController::class, 'show']);
+// Route::post('/Book/{id}', [BookController::class, 'update']);
+// Route::delete('/Book/{id}', [BookController::class, 'destroy']);
+
+Route::apiResource('/Author', [AuthorController::class,]);
+Route::apiResource('/Genre', [AuthorController::class,]);
+Route::apiResource('/Book', [AuthorController::class,]);
 
 
